8681bc2485eea920cf3d2d565e3ca62b8628c0be
Danaipat Sodkomkham <sdanaipat@gmail.com>
2020-11-30 23:03:20 +0900

    reorg

--- setup.py
+++ setup.py
@@ -9,27 +9,22 @@ setup(
     description='get latest stock price from Yahoo! Finance',
     author='Danaipat Sodkomkham',
     author_email='sdanaipat@gmail.com',
-    url='https://github.com/user/reponame',
-
-    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
-    # Keywords that define your package best
-    keywords=['SOME', 'MEANINGFULL', 'KEYWORDS'],
-    install_requires=[            # I get to this in a second
-        'validators',
+    url='https://github.com/sdanaipat/kabuka',
+    download_url='https://github.com/sdanaipat/kabuka/archive/v_01.tar.gz',
+    keywords=['stock price', 'yahoo finance', 'market price', 'get latest stock price'],
+    install_requires=[
+        'requests',
         'beautifulsoup4',
     ],
     classifiers=[
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Development Status :: 3 - Alpha',
-        # Define that your audience are developers
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
-        # Specify which pyhton versions that you want to support
+        'Topic :: Financial Data API',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
-view rawsetup.py hosted with ❤ by GitHub
